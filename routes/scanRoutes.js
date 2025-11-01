b421e34b48b32ccea124ad1ae76d65fc25655432
stickaeg <stickaeg.de@gmail.com>
2025-11-01 22:48:45 +0200

    manualFulfill

--- routes/scanRoutes.js
+++ routes/scanRoutes.js
@@ -5,11 +5,31 @@ const {
   scanBatch,
   scanUnitFulfillment,
 } = require("../controllers/scan.controllers");
+const prisma = require("../prisma/client");
+const { fulfillOrder } = require("../services/shopifyServices");
 
 // Batch QR scan (Printer)
 router.get("/batch/:token", scanBatch);
 
 // Item QR scan (Fulfillment)
 router.get("/item-fulfillment/:token", scanUnitFulfillment);
+
+router.post("/manualFulfill/:orderId", async (req, res) => {
+  const order = await prisma.order.findUnique({
+    where: { id: parseInt(req.params.orderId) },
+    include: { store: true },
+  });
+
+  try {
+    await fulfillOrder(
+      order.store.shopDomain,
+      order.store.accessToken,
+      order.shopifyId
+    );
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
